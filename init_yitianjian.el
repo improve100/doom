5eceec7db41b95f4de2738b72d8fc50b9a113b19
tong <939896399@qq.com>
2023-04-25 22:05:06 +0800

    yitianjian config packages

--- init_yitianjian.el
+++ init_yitianjian.el
@@ -98,7 +98,8 @@
        lookup              ; navigate your code and its documentation
        ;; lsp               ; M-x vscode
        lsp                ; M-x vscode
-       magit             ; a git porcelain for Emacs
+       (magit +forge)             ; a git porcelain for Emacs
+       ;; magit               ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
